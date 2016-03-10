8c6e5b9fb8e0b17c9b210863bf2d15cd868645a3
Martin Savko <martin.savko@synchrotron-soleil.fr>
2016-03-10 15:10:57 +0100

    Refactoring code to support for easier parallel processing.
    Adding support for parallel extraction (using python multiprocessing module)
    
    On my 32 cpu system the speed of extraction increases by factor of 50 (going down to ~0.05s per image).
    
    Number of cpu to use in parallel may be specified as an option. By default the maximum available number as given by multiprocessing.cpu_count() will be used.
    
    First and last image to extract may be now optionally specified.

--- H5ToCBF.py
+++ H5ToCBF.py
@@ -21,6 +21,7 @@ import h5py
 import time 
 import os
 import shutil
+import multiprocessing
 
 header_template = '''###CBF: VERSION 1.5, CBFlib v0.7.8 - SLS/DECTRIS PILATUS detectors
 
@@ -117,6 +118,15 @@ def get_header_information(master_file):
     h['oscillation_axis'] = oscillation_axis
     h['start_angle'] = None
     h['angle_increment'] = master_file[oscillation_axes_ranges[oscillation_axis]].value
+    h['omegas'] = master_file["/entry/sample/goniometer/omega"].value
+    h['phis'] = master_file["/entry/sample/goniometer/phi"].value
+    h['kappas'] = master_file["/entry/sample/goniometer/kappa"].value
+    h['chis'] = master_file["/entry/sample/goniometer/chi"].value
+    h['oscillation_axis_values'] = master_file["/entry/sample/goniometer/%s" % oscillation_axis.lower()].value
+    image_path = os.path.dirname(os.path.abspath(master_file.filename))
+    h['image_path'] = image_path
+    filename_template = master_file.filename.replace('_master.h5', '_#####.cbf')
+    h['filename_template'] = filename_template
     return h
 
 def get_oscillation_axis(master_file):
@@ -130,67 +140,98 @@ def get_oscillation_axis(master_file):
         return 'CHI'
     return 'OMEGA'
     
-def extract_cbfs(master_file, start_dir):
+def get_single_wedge(start, images_in_wedge):
+    return [start + j for j in range(images_in_wedge)]
+        
+def get_wedges(start, nimages, n_cpu):
+    iterations, rest = divmod(nimages, n_cpu)
+    wedges = []
+    for i in range(iterations):
+        wedges.append(get_single_wedge(start+i*n_cpu, n_cpu))
+    if rest:
+        wedges.append(get_single_wedge(start+iterations*n_cpu, rest))
+    return wedges
+
+def get_nimages(master_file, first, last):
     nimages = master_file["/entry/instrument/detector/detectorSpecific/nimages"].value
-    
-    oscillation_axis = get_oscillation_axis(master_file)
-    
-    omegas = master_file["/entry/sample/goniometer/omega"].value
-    phis = master_file["/entry/sample/goniometer/phi"].value
-    kappas = master_file["/entry/sample/goniometer/kappa"].value
-    chis = master_file["/entry/sample/goniometer/chi"].value
-    oscillation_axis_values = master_file["/entry/sample/goniometer/%s" % oscillation_axis.lower()].value
+    if first!=0 and last!=-1 and last>first:
+        nimages = last - first
+    elif first > 0:
+        nimages = nimages - first
+    elif last > 0 and last>first:
+        nimages = last - first
+    elif last < 0:
+        last = nimages + last
+        nimages = last - first + 1
+    return nimages
+
+def extract_cbfs(master_file, start_dir, first=0, last=-1, n_cpu=0):
+    nimages = get_nimages(master_file, first, last)
     
     header_dictionary = get_header_information(master_file)
-    image_path = os.path.dirname(os.path.abspath(master_file.filename))
-    
-    header_dictionary['image_path'] = image_path
-    filename_template = master_file.filename.replace('_master.h5', '_#####.cbf')
     
     start = time.time()
-    for n in range(nimages):
-        filename = os.path.basename(filename_template.replace('#####', str(n+1).zfill(5)))
-        header_dictionary['filename'] = 'data_%s'  % (filename.replace('.cbf',''))
-        try:
-            if type(omegas) == float:
-                header_dictionary['omega'] = omegas
-                header_dictionary['phi'] = phis
-                header_dictionary['kappa'] = kappas
-                header_dictionary['chi'] = chis
-                header_dictionary['start_angle'] = oscillation_axis_values
-            else:
-                header_dictionary['omega'] =  omegas[n]
-                header_dictionary['phi'] = phis[n]
-                header_dictionary['kappa'] = kappas[n]
-                header_dictionary['chi'] = chis[n]
-                header_dictionary['start_angle'] = oscillation_axis_values[n]
-        except IndexError:
-            print 'oscillation_axis_values', oscillation_axis_values
-            header_dictionary['omega'] =  oscillation_axis_values
+    
+    if n_cpu <= 0:
+        n_cpu = multiprocessing.cpu_count()
+    
+    wedges = get_wedges(first, nimages, n_cpu)
+    for wedge in wedges:
+        jobs = []
+        for num in wedge:
+            p = multiprocessing.Process(target=save_image, args=(header_dictionary, num))
+            jobs.append(p)
+            p.start()
+        for job in jobs:
+            job.join()
+    
+    end = time.time()
+    print 'total processing time %6.4f s, which is %6.4f s per image' % (end-start, (end-start)/nimages)
+        
+def save_image(header_dictionary, n):
+    filename_template = header_dictionary['filename_template']
+    filename = os.path.basename(filename_template.replace('#####', str(n+1).zfill(5)))
+    header_dictionary['filename'] = 'data_%s'  % (filename.replace('.cbf',''))
+    omegas = header_dictionary['omegas']
+    phis = header_dictionary['phis']
+    kappas = header_dictionary['kappas']
+    chis = header_dictionary['chis']
+    oscillation_axis_values = header_dictionary['oscillation_axis_values']
+    image_path = header_dictionary['image_path']
+    try:
+        if type(omegas) == float:
+            header_dictionary['omega'] = omegas
             header_dictionary['phi'] = phis
             header_dictionary['kappa'] = kappas
             header_dictionary['chi'] = chis
             header_dictionary['start_angle'] = oscillation_axis_values
+        else:
+            header_dictionary['omega'] =  omegas[n]
+            header_dictionary['phi'] = phis[n]
+            header_dictionary['kappa'] = kappas[n]
+            header_dictionary['chi'] = chis[n]
+            header_dictionary['start_angle'] = oscillation_axis_values[n]
+    except IndexError:
+        print 'oscillation_axis_values', oscillation_axis_values
+        header_dictionary['omega'] = oscillation_axis_values
+        header_dictionary['phi'] = phis
+        header_dictionary['kappa'] = kappas
+        header_dictionary['chi'] = chis
+        header_dictionary['start_angle'] = oscillation_axis_values
 
-        header = header_template.format(**header_dictionary)
-        header_filename = 'header_%s' % (str(n+1).zfill(5))
-        f = open(header_filename, 'w')
-        f.write(header)
-        f.close()
+    header = header_template.format(**header_dictionary)
+    header_filename = 'header_%s' % (str(n+1).zfill(5))
+    f = open(header_filename, 'w')
+    f.write(header)
+    f.close()
+    
+    raw_cbf_filename = '%s.cbf' % str(n+1).zfill(5)
+    os.system('H5ToXds %s %s %s' % (os.path.join(image_path, master_file.filename), n+1, raw_cbf_filename))
+    os.system('cat %s | tail -n +14 >> %s' % (raw_cbf_filename, header_filename))
+    
+    shutil.move(header_filename, os.path.join(start_dir, filename))
+    os.remove(raw_cbf_filename)
         
-        raw_cbf_filename = '%s.cbf' % str(n+1).zfill(5)
-        os.system('H5ToXds %s %s %s' % (os.path.join(image_path, master_file.filename), n+1, raw_cbf_filename))
-        os.system('cat %s | tail -n +14 >> %s' % (raw_cbf_filename, header_filename))
-        
-        shutil.move(header_filename, os.path.join(start_dir, filename))
-        os.remove(raw_cbf_filename)
-        if n % 100 == 0:
-            t = time.time() - start
-            print('extracting image #%s, total time %s, time per image %s' % (str(n+1).zfill(5), t, t/(n+1)))
-            print header_dictionary
-        if (n+1) == nimages:
-            t = time.time() - start
-            print('Extracted %s images, total time %s, time per image %s' % (nimages, t, t/(n+1)))
             
 if __name__ == '__main__':
     import optparse
@@ -198,6 +239,9 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     
     parser.add_option('-m', '--master_file', type=str, help='Path to the master_file')
+    parser.add_option('-n', '--n_cpu', default=0, type=int, help='Number of parallel extraction, by defalult it will determine the number of cores of the machine and use all of them.')
+    parser.add_option('-f', '--first', default=0, type=int, help='First image to extract. Default is the first one.')
+    parser.add_option('-l', '--last', default=-1, type=int, help='Last image to extract. Default is the last one.')
     
     options, args = parser.parse_args()
     
@@ -207,7 +251,7 @@ if __name__ == '__main__':
     
     master_file = h5py.File(os.path.basename(options.master_file))
     
-    extract_cbfs(master_file, start_dir)
+    extract_cbfs(master_file, start_dir, options.first, options.last, options.n_cpu)
     
     os.chdir(start_dir)
     
