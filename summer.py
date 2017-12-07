579dadb0ab6887d49a217f1fe7805995dd49ae00
Vicente Rey Bakaikoa <txo@txolutions.com>
2017-12-07 15:53:36 +0100

    New options in summer.py. It is now possible to simmulate loosing random or specific images (e.g. for dead time increase simulation).
    Adding command line options to make use of new features.

--- summer.py
+++ summer.py
@@ -9,6 +9,7 @@ import traceback
 import numpy as np
 import bitshuffle.h5
 import math
+import random
 
 def create_new_master(reference_master, new_name):
     shutil.copy(reference_master, new_name)
@@ -33,7 +34,28 @@ def get_cube(master, images_to_sum, images_per_file=None):
         del reblock
     return cube
 
-def sum_and_save_not_pretending_to_be_smart(master, images_to_sum, images_per_file):
+def loose_the_image(image_index, images_to_loose, indices_of_images_to_loose):
+    if images_to_loose == 0:
+        return False
+    elif image_index in indices_of_images_to_loose:
+        return True
+    else:
+        return False
+    
+def get_ill(indices_of_images_to_loose, images_to_sum):
+    if indices_of_images_to_loose == 'random':
+        wedge_indices = range(images_to_sum)
+        iil = []
+        for k in range(images_to_loose):
+            iil.append(wedge_indices.pop(random.choice(wedge_indices)))
+        print 'indices_of_images_to_loose', indices_of_images_to_loose
+    elif images_to_sum == len(indices_of_images_to_loose):
+        iil = indices_of_images_to_loose[:]
+    else:
+        iil = []
+    return iil
+        
+def sum_and_save_not_pretending_to_be_smart(master, images_to_sum, images_per_file, images_to_loose, indices_of_images_to_loose):
     datakeys = master['/entry/data'].keys()
     datakeys.sort()
     
@@ -49,16 +71,25 @@ def sum_and_save_not_pretending_to_be_smart(master, images_to_sum, images_per_fi
     
     for key in datakeys:
         print key
-        block = master['/entry/data/%s' % key].value
+        try:
+            block = master['/entry/data/%s' % key].value
+        except KeyError:
+            continue
         if key == datakeys[0]:
             dtype = block.dtype
             nimages, image_height, image_width = block.shape
-            new_image = np.zeros((image_height, image_width))
-        
+            new_image = np.zeros((image_height, image_width), dtype=np.int64)
+            
         for image in block:
+            if images_to_loose > 0 and since_last_summed == 0:
+                iil = get_iil(indices_of_images_to_loose, images_to_sum)
+            else:
+                iil = []
+                
             since_last_summed += 1
             
-            new_image = np.add(new_image, image)
+            if not loose_the_image(since_last_summed, images_to_loose, iil):
+                new_image = np.add(new_image, image)
             
             if since_last_summed == images_to_sum:
                 new_image = new_image.reshape((1, image_height, image_width))
@@ -69,7 +100,7 @@ def sum_and_save_not_pretending_to_be_smart(master, images_to_sum, images_per_fi
                 print 'summed.shape', summed.shape
                 new_image = np.zeros((image_height, image_width))
                 since_last_summed = 0
-                
+                    
             if len(summed) == images_per_file:
                 print 'len(summed)', len(summed)
                 datafile_number += 1
@@ -200,16 +231,31 @@ def main():
     
     parser = optparse.OptionParser()
     
-    parser.add_option('-m', '--master_file', type=str, default='collect_1_master.h5', help='master file to handle')
-    parser.add_option('-n', '--images_to_sum', type=int, default=2, help='number of images to sum')
+    parser.add_option('-m', '--master_file', type=str, default='collect_1_master.h5', help='master file')
+    parser.add_option('-N', '--new_master_file', type=str, default=None, help='new master filename')
+    parser.add_option('-n', '--images_to_sum', type=int, default=None, help='number of images to sum')
     parser.add_option('-p', '--images_per_file', type=int, default=10, help='number of images per data file')
+    parser.add_option('-l', '--images_to_loose', type=int, default=0, help='number of original images not to include in the new images -- useful for simulating increased deadtime or random loss of images')
+    parser.add_option('-i', '--indices_of_images_to_loose', type=str, default='-1', help='String specifying what images not to include. Depends on the nimages_to_loose value. Either integer, string that will evaluate to python tuple or list or "random" string if images are to be chosen randomly')
     
     options, args = parser.parse_args()
     
-    images_to_sum = options.images_to_sum
+    if options.images_to_sum != None:
+        images_to_sum = options.images_to_sum
+    else:
+        images_to_sum = 'all'
     images_per_file = options.images_per_file
+    images_to_loose = options.images_to_loose
+    indices_of_images_to_loose = options.indices_of_images_to_loose
     
-    new_name = options.master_file.replace('_master.h5', '_bak_sum%d_master.h5' % options.images_to_sum)
+    if images_to_loose > 0:
+        if indices_of_images_to_loose != 'random':
+            indices_of_images_to_loose = eval(indices_of_images_to_loose)
+
+    if options.new_master_file == None:
+        new_name = options.master_file.replace('_master.h5', '_sum%s_master.h5' % str(images_to_sum))
+    else:
+        new_name = options.new_master_file
 
     create_new_master(options.master_file, new_name)
     
@@ -232,8 +278,10 @@ def main():
                             
     #recube = get_cube(m, images_to_sum)
     #new_nimages, image_height, image_width = recube.shape
-    
-    new_nimages, data_filenames = sum_and_save_not_pretending_to_be_smart(new_m, images_to_sum, images_per_file)
+    if images_to_sum == 'all':
+        images_to_sum = m['/entry/instrument/detector/detectorSpecific/nimages'].value
+        
+    new_nimages, data_filenames = sum_and_save_not_pretending_to_be_smart(new_m, images_to_sum, images_per_file, images_to_loose, indices_of_images_to_loose)
     
     print 'new_nimages', new_nimages
     print 'data_filenames', data_filenames
@@ -254,39 +302,42 @@ def main():
         print 'new_value', new_value
         new_m[pm].write_direct(new_value)
         print 'confirm', new_m[pm].value
-        
-    for angle in angles_to_modify:
-        print 'angle', angle
-        path = angles_to_modify[angle]
-        start = m['%s/%s_start' % (path, angle)].value
-        total = m['%s/%s_range_total' % (path, angle)].value
-        increment = m['%s/%s_increment' % (path, angle)].value
-        print 'start', start
-        print 'total', total
-        print 'increment', increment
-        
-        if increment != 0:
-            new_increment = np.array([increment * images_to_sum])
-            print 'new_increment', new_increment
-            new_m['%s/%s_increment' % (path, angle)].write_direct(new_increment)
-            range_average = m['%s/%s_range_average' % (path, angle)].value
-            new_range_average = np.array([range_average * images_to_sum])
-            new_m['%s/%s_range_average' % (path, angle)].write_direct(new_range_average)
-            new_starts = np.arange(start, total+start, increment*images_to_sum)
-            new_ends = new_starts + new_increment
-        else:
-            new_starts = np.zeros(new_nimages)
-            new_ends = new_starts
+      
+    try:
+        for angle in angles_to_modify:
+            print 'angle', angle
+            path = angles_to_modify[angle]
+            start = m['%s/%s_start' % (path, angle)].value
+            total = m['%s/%s_range_total' % (path, angle)].value
+            increment = m['%s/%s_increment' % (path, angle)].value
+            print 'start', start
+            print 'total', total
+            print 'increment', increment
             
-        dtype = m['%s/%s' % (path, angle)].dtype
-        del new_m['%s/%s' % (path, angle)]
-        
-        new_m.create_dataset('%s/%s' % (path, angle), data=new_starts, dtype=dtype)
-        del new_m['%s/%s_end' % (path, angle)]
-        new_m.create_dataset('%s/%s_end' % (path, angle), data=new_ends, dtype=dtype)
-        
-        print new_m['%s/%s' % (path, angle)].value
-        print new_m['%s/%s_end' % (path, angle)].value
+            if increment != 0:
+                new_increment = np.array([increment * images_to_sum])
+                print 'new_increment', new_increment
+                new_m['%s/%s_increment' % (path, angle)].write_direct(new_increment)
+                range_average = m['%s/%s_range_average' % (path, angle)].value
+                new_range_average = np.array([range_average * images_to_sum])
+                new_m['%s/%s_range_average' % (path, angle)].write_direct(new_range_average)
+                new_starts = np.arange(start, total+start, increment*images_to_sum)
+                new_ends = new_starts + new_increment
+            else:
+                new_starts = np.zeros(new_nimages)
+                new_ends = new_starts
+                
+            dtype = m['%s/%s' % (path, angle)].dtype
+            del new_m['%s/%s' % (path, angle)]
+            
+            new_m.create_dataset('%s/%s' % (path, angle), data=new_starts, dtype=dtype)
+            del new_m['%s/%s_end' % (path, angle)]
+            new_m.create_dataset('%s/%s_end' % (path, angle), data=new_ends, dtype=dtype)
+            
+            print new_m['%s/%s' % (path, angle)].value
+            print new_m['%s/%s_end' % (path, angle)].value
+    except Exception as e:
+        print traceback.print_exc()
         
     m.close()
     
