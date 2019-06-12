8427e81875724e303c924f8343710fac02852648
smartin <savko@synchrotron-soleil.fr>
2019-06-12 15:14:00 +0200

    detector_beamstop.py  -- move to tango_motor instead of direct PyTango access

--- detector_beamstop.py
+++ detector_beamstop.py
@@ -1,14 +1,16 @@
-import PyTango
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from motor import tango_motor
 
 class detector_beamstop:
     def __init__(self):
-        self.mt_x = PyTango.DeviceProxy('i11-ma-cx1/ex/bst.3-mt_tx')
-        self.mt_z = PyTango.DeviceProxy('i11-ma-cx1/ex/bst.3-mt_tz')
+        self.mt_x = tango_motor('i11-ma-cx1/ex/bst.3-mt_tx')
+        self.mt_z = tango_motor('i11-ma-cx1/ex/bst.3-mt_tz')
     def set_z(self, position):
-        self.mt_z.position = position
+        self.mt_z.set_position(position)
     def set_x(self, position):
-        self.mt_z.position = position
+        self.mt_z.set_position(position)
     def get_z(self):
-        return self.mt_z.position
+        return self.mt_z.get_position()
     def get_x(self):
-        return self.mt_x.position
+        return self.mt_x.get_position()
