9c5886f6860237eb3435b3fd8acc39110e1cd6e2
smartin <savko@synchrotron-soleil.fr>
2019-06-13 07:48:08 +0200

    -- more careful closing and opening

--- safety_shutter.py
+++ safety_shutter.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import PyTango
 import time
+import gevent
 from md2_mockup import md2_mockup
 
 class obx_mockup:
@@ -29,14 +30,20 @@ class safety_shutter(object):
         start = time.time()
         if self.security.prmObt == 1 and self.state != 'OPEN':
             self.shutter.Open()
-            while self.state != 'OPEN' and abs(time.time()-start) < timeout:
-                time.sleep(checktime)
+            while self.state() != 'OPEN' and abs(time.time()-start) < timeout:
+                gevent.sleep(checktime)
                 self.shutter.Open()
         elif self.security.prmObt != 1:
             print 'Not possible to open the safety shutter due to a security issue. Has the hutch been searched and locked?'
         
-    def close(self):
-        return self.shutter.Close()
+    def close(self, checktime=2., timeout=10.):
+        start = time.time()
+        while not self.closed() and time.time() - start < timeout:
+            try:
+                self.shutter.Close()
+            except:
+                pass
+            gevent.sleep(checktime)
     
     def state(self):
         return self.shutter.State().name
