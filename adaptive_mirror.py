81ef1ada47c044ecdc849e7752dbf89404bc6456
Martin Savko <savko@synchrotron-soleil.fr>
2025-06-15 17:14:13 +0200

    Move adaptive_mirror class to a separate file.

---
+++ adaptive_mirror.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import numpy as np
+try:
+    import urllib2
+except ImportError:
+    import urllib.request as urllib2
+try:
+    import tango
+except ImportError:
+    import PyTango as tango
+
+from motor import tango_motor
+
+class adaptive_mirror(object):
+    
+    mirror_address = {'vfm': 'i11-ma-c05/op/mir2-vfm', 'hfm': 'i11-ma-c05/op/mir3-hfm'}
+    reset_page = 'http://bimorph:8080/cgi-bin/rackconfig_user/channeltest'
+    
+    def __init__(self, mirror='vfm', check_time=1.):
+        
+        self.mirror = mirror
+        self.check_time = check_time
+        self.mirror_device = tango.DeviceProxy(self.mirror_address[self.mirror])
+        channel_base = self.mirror_address[mirror].replace(mirror, 'ch.%02d')
+        self.channels = [tango.DeviceProxy(channel_base % k) for k in range(12)]
+        self.pitch = tango_motor(self.mirror_address[mirror].replace('mir2-vfm', 'mir.2-mt_rx').replace('mir3-hfm', 'mir.3-mt_rz'))
+        self.translation = tango_motor(self.mirror_address[mirror].replace('mir2-vfm', 'mir.2-mt_tz').replace('mir3-hfm', 'mir.3-mt_tx'))
+        
+    def get_channel_values(self):
+        return [getattr(c, 'voltage') for c in self.channels]
+    
+    def get_channel_target_values(self):
+        return [getattr(c, 'targetVoltage') for c in self.channels]
+    
+    def set_channel_target_values(self, channel_values, number_of_channels=12):
+        if len(channel_values) == 0:
+            print('not modifying target values as none specified')
+        elif len(channel_values) != number_of_channels:
+            print('not modifying target values as the value vector length is not consistent with number of channels (%d)' % number_of_channels)
+        else:
+            for k, c in enumerate(channel_values):
+                if c not in [None, np.nan]:
+                    setattr(self.channels[k], 'targetVoltage', c)
+                    time.sleep(1)
+                else:
+                    print('not modifying channel %+d, current value %.1f' % (k, getattr(c, 'voltage')))
+        print('current target voltages: %s' % self.get_channel_target_values())
+        
+    def set_voltages(self, channel_values):
+        current_channel_values = self.get_channel_values()
+        _start = time.time()
+        if np.allclose(channel_values, current_channel_values):
+            print('current channel values', current_channel_values)
+            print('values requested are identical, moving on ...')
+            
+        else:
+            print('Setting %s mirror voltages' % self.mirror)
+            print('Please wait for %s mirror tensions to settle ...' % self.mirror)
+            self.set_channel_target_values(channel_values)
+            self.mirror_device.SetChannelsTargetVoltage()
+            while not np.allclose(channel_values, self.get_channel_values()):
+                time.sleep(self.check_time)
+            self.wait()
+        _end = time.time()
+        print()
+        print('done!')
+        print('%s mirror tensions converged' % self.mirror)
+        
+        print('set_voltages() took %.2f' % (_end-_start))
+        
+    def wait(self):
+        while self.mirror_device.State().name != 'STANDBY':
+            time.sleep(self.check_time)
+        
+    def get_pitch_position(self):
+        return self.pitch.get_position()
+    
+    def get_translation_position(self):
+        return self.translation.get_position()
+    
+    def set_pitch_position(self, position):
+        self.pitch.set_position(position)
+        
+    def set_translation_position(self, position):
+        self.translation.set_position(position)
+        
+    def reload_firmware(self, key='cG93ZXJ1c2VyOnBvd2VydXNlcg=='):
+        req = urllib2.Request(self.reset_page)
+        req.add_header("Authorization", "Basic %s" % key)
+        handle = urllib2.urlopen(req)
+        return handle
+    
+    def get_position(self):
+        position = {
+            "pitch": self.get_pitch_position(),
+            "translation": self.get_translation_position(),
+        }
+        return position
+    
--- mirror_scan.py
+++ mirror_scan.py
@@ -4,30 +4,22 @@
 Slits scan. Execute scan on a pair of slits.
 
 '''
-import gevent
+
+import os
 import sys
-
-try:
-    import tango
-except ImportError:
-    import PyTango as tango
-
+import gevent
 import traceback
 import logging
 import time
 import itertools
-import os
 import pickle
 import numpy as np
 import pylab
 import glob
-import itertools
-
-from xray_experiment import xray_experiment
 from scipy.constants import eV, h, c, angstrom, kilo, degree
 
-from motor import tango_motor
 from monitor import xray_camera, analyzer
+from adaptive_mirror import adaptive_mirror
 from camera import camera
 from redis import StrictRedis
 from slit_scan import slit_scan
@@ -78,98 +70,6 @@ class mirror_scan_analysis(slit_scan_analysis):
             
         self.save_results(results)
 
-try:
-    import urllib2
-except ImportError:
-    import urllib.request as urllib2
-import base64
-
-class adaptive_mirror(object):
-    
-    mirror_address = {'vfm': 'i11-ma-c05/op/mir2-vfm', 'hfm': 'i11-ma-c05/op/mir3-hfm'}
-    reset_page = 'http://bimorph:8080/cgi-bin/rackconfig_user/channeltest'
-    
-    def __init__(self, mirror='vfm', check_time=1.):
-        
-        self.mirror = mirror
-        self.check_time = check_time
-        self.mirror_device = tango.DeviceProxy(self.mirror_address[self.mirror])
-        channel_base = self.mirror_address[mirror].replace(mirror, 'ch.%02d')
-        self.channels = [tango.DeviceProxy(channel_base % k) for k in range(12)]
-        self.pitch = tango_motor(self.mirror_address[mirror].replace('mir2-vfm', 'mir.2-mt_rx').replace('mir3-hfm', 'mir.3-mt_rz'))
-        self.translation = tango_motor(self.mirror_address[mirror].replace('mir2-vfm', 'mir.2-mt_tz').replace('mir3-hfm', 'mir.3-mt_tx'))
-        
-    def get_channel_values(self):
-        return [getattr(c, 'voltage') for c in self.channels]
-    
-    def get_channel_target_values(self):
-        return [getattr(c, 'targetVoltage') for c in self.channels]
-    
-    def set_channel_target_values(self, channel_values, number_of_channels=12):
-        if len(channel_values) == 0:
-            print('not modifying target values as none specified')
-        elif len(channel_values) != number_of_channels:
-            print('not modifying target values as the value vector length is not consistent with number of channels (%d)' % number_of_channels)
-        else:
-            for k, c in enumerate(channel_values):
-                if c not in [None, np.nan]:
-                    setattr(self.channels[k], 'targetVoltage', c)
-                    gevent.sleep(1)
-                else:
-                    print('not modifying channel %+d, current value %.1f' % (k, getattr(c, 'voltage')))
-        print('current target voltages: %s' % self.get_channel_target_values())
-        
-    def set_voltages(self, channel_values):
-        current_channel_values = self.get_channel_values()
-        _start = time.time()
-        if np.allclose(channel_values, current_channel_values):
-            print('current channel values', current_channel_values)
-            print('values requested are identical, moving on ...')
-            
-        else:
-            print('Setting %s mirror voltages' % self.mirror)
-            print('Please wait for %s mirror tensions to settle ...' % self.mirror)
-            self.set_channel_target_values(channel_values)
-            self.mirror_device.SetChannelsTargetVoltage()
-            while not np.allclose(channel_values, self.get_channel_values()):
-                gevent.sleep(self.check_time)
-            self.wait()
-        _end = time.time()
-        print()
-        print('done!')
-        print('%s mirror tensions converged' % self.mirror)
-        
-        print('set_voltages() took %.2f' % (_end-_start))
-        
-    def wait(self):
-        while self.mirror_device.State().name != 'STANDBY':
-            gevent.sleep(self.check_time)
-        
-    def get_pitch_position(self):
-        return self.pitch.get_position()
-    
-    def get_translation_position(self):
-        return self.translation.get_position()
-    
-    def set_pitch_position(self, position):
-        self.pitch.set_position(position)
-        
-    def set_translation_position(self, position):
-        self.translation.set_position(position)
-        
-    def reload_firmware(self, key='cG93ZXJ1c2VyOnBvd2VydXNlcg=='):
-        req = urllib2.Request(self.reset_page)
-        req.add_header("Authorization", "Basic %s" % key)
-        handle = urllib2.urlopen(req)
-        return handle
-    
-    def get_position(self):
-        position = {
-            "pitch": self.get_pitch_position(),
-            "translation": self.get_translation_position(),
-        }
-        return position
-    
         
 class mirror_scan(slit_scan):
     
