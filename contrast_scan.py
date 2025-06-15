f0f4d8e1ece8d8b4b4b8f971503f45f1a92af8d6
Martin Savko <savko@synchrotron-soleil.fr>
2025-06-15 17:32:09 +0200

    contrast scan

--- contrast_scan.py
+++ contrast_scan.py
@@ -12,13 +12,13 @@ def main():
     cam = camera()
     g = goniometer()
     
-    x = np.linspace(-0.1, 0.1, 50)
+    x = np.linspace(-0.2, 0.6, 50)
     #x = x[::-1]
     contrasts = []
     
     for p in x:
         g.set_position({'AlignmentX': p}, wait=True)
-        time.sleep(0.25)
+        time.sleep(0.01)
         img = cam.get_image()
         contrasts.append(cam.get_contrast(image=img[300:-300, 400:-400]))
         
