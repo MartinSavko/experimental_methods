a3c0c78424f0e5c0db4e674e7dde86ca114b83be
Martin Savko <savko@synchotron-soleil.fr>
2025-09-26 19:45:38 +0200

    rectified_projections determined in get_projections_from_profiles get_overlay

--- shape_from_vadt.py
+++ shape_from_vadt.py
@@ -24,6 +24,7 @@ from scipy.interpolate import interp1d, RectBivariateSpline
 from scipy.spatial import distance_matrix
 
 from useful_routines import (
+    get_shift_from_aligned_position_and_reference_position,
     get_aligned_position_from_reference_position_and_shift,
     get_shift_between_positions,
     get_points_in_goniometer_frame,
@@ -74,7 +75,7 @@ def get_profiles(
     lines,
     scan_start_angles,
     threshold_identity=0.999,
-    #threshold_projection=0.7,
+    # threshold_projection=0.7,
     threshold_projection=0.85,
 ):
     print("scan_start_angles", scan_start_angles)
@@ -179,8 +180,76 @@ def get_rasters_from_profiles(
     return rasters
 
 
+def get_overlay(
+    optical_image,
+    raster,
+    optical_reference,
+    raster_reference,
+    optical_calibration,
+    raster_calibration,
+):
+    # overlay = optical_image.copy() #.mean(axis=2)
+    print("optical shape", optical_image.shape[:2])
+    print("raster shape", raster.shape)
+    print("optical_calibration", optical_calibration)
+    print("raster_calibration", raster_calibration)
+    overlay = optical_image.mean(axis=2)
+    shift_mm = get_shift_from_aligned_position_and_reference_position(
+        raster_reference, optical_reference
+    )
+    shift_px = shift_mm / optical_calibration
+    print("shift (mm, px):", shift_mm, shift_px)
+    rV, rH = raster.shape
+    oV, oH = optical_image.shape[:2]
+    scale = raster_calibration / optical_calibration  # /raster_calibration
+    print("scale", scale)
+    optical_raster = cv.resize(
+        raster,
+        (
+            int(rH * scale[1]),
+            int(rV * scale[0]),
+        ),
+    )
+    center = np.array([oV / 2, oH / 2])
+    or_shape = np.array(optical_raster.shape)
+    print("optical raster shape", or_shape)
+    raster_start = center - or_shape / 2 + shift_px
+    sV, sH = raster_start.astype(int)
+    # eV, eH = (raster_start + or_shape + 1).astype(int)
+    print("raster_start", raster_start)
+    print("raster_extent", or_shape)
+    eV = sV + or_shape[0]
+    eH = sH + or_shape[1]
+    print("eV -sV, eH - sH", eV - sV, eH - sH)
+    assert or_shape[0] == eV - sV
+    assert or_shape[1] == eH - sH
+    # overlay[sV: eV, sH: eH] = optical_raster
+    opr = 255 * optical_raster / optical_raster.max()
+    overlay[sV:eV, sH:eH][optical_raster > 25] = opr[optical_raster > 25]
+    return overlay
+
+
+def get_rectified_projection(rectified_interpolation, along_cells):
+    ortho_cells = rectified_interpolation.shape[1]
+    rectified_projection = cv.resize(
+        rectified_interpolation,
+        (
+            ortho_cells,
+            along_cells,
+        ),
+    )
+    return rectified_projection
+
+
 def get_projections_from_profiles(
-    profiles, nimages, seed_positions, max_bounding_ray, reference_position, oa
+    profiles,
+    nimages,
+    seed_positions,
+    max_bounding_ray,
+    reference_position,
+    oa,
+    along_cells,
+    ortho_step,
 ):
     rasters = get_rasters_from_profiles(
         profiles, seed_positions, max_bounding_ray, reference_position
@@ -191,7 +260,10 @@ def get_projections_from_profiles(
     rectification_stop = stop_shifts_all[np.argmax(np.abs(stop_shifts_all))]
 
     omegas, images = oa._get_omegas_images()
-    
+    optical_reference = oa.get_reference_position()
+    optical_calibration = oa.get_calibration()
+    raster_calibration = np.array([ortho_step, ortho_step])
+
     projections = {}
     for angle in profiles:
         image = oa.get_image_at_angle(angle, omegas=omegas, images=images)
@@ -208,12 +280,26 @@ def get_projections_from_profiles(
             rectification_start,
             rectification_stop,
         )
+
+        rectified_projection = get_rectified_projection(
+            rectified_interpolation.T, along_cells
+        )
+
         projections[angle] = {
             "measurement": measurement,
             "estimation": estimation,
             "interpolation": interpolation,
             "rectified_interpolation": rectified_interpolation,
+            "rectified_projection": rectified_projection,
             "optical_image": image,
+            "overlay": get_overlay(
+                image,
+                rectified_projection,
+                optical_reference,
+                reference_position,
+                optical_calibration,
+                raster_calibration,
+            ),
         }
 
     return projections
@@ -282,26 +368,29 @@ def get_projection(
     ip2 = RectBivariateSpline(x, y, rectified)
     y = np.arange(nlines)
     rectified_interpolation = ip2(x, y)[::-1]
-    
+
     return measurement, estimation, interpolation, rectified_interpolation
 
 
 def plot_projections(projections, ntrigger, nimages, along_step, ortho_step):
+    keys = [
+        "measurement",
+        "estimation",
+        "interpolation",
+        "rectified_interpolation",
+        "optical_image",
+        "overlay",
+    ]
+    columns = len(keys)
     norientations = len(projections)
-    fig, axes = pylab.subplots(math.ceil(5 * norientations / 5), 5)
+    fig, axes = pylab.subplots(math.ceil(columns * norientations / columns), columns)
     fig.suptitle("evidence")
     axs = axes.ravel()
     k = 0
     for angle in projections:
-        for key in [
-            "measurement",
-            "estimation",
-            "interpolation",
-            "rectified_interpolation",
-            "optical_image",
-        ]:
+        for key in keys:
             p = projections[angle][key]
-            if key not in ["optical_image"]:
+            if key not in ["optical_image", "overlay"]:
                 fimages = p.shape[0]
                 p = cv.resize(
                     p,
@@ -320,39 +409,18 @@ def plot_projections(projections, ntrigger, nimages, along_step, ortho_step):
             axs[k].set_axis_off()
             k += 1
 
-def plot_rectified_projections(
-    projections, angles, ntrigger, nimages, along_range, ortho_step, threshold=25
-):
-    norientations = len(projections)
-    fig, axes = pylab.subplots(math.ceil(3 * norientations / 3), 3)
-    fig.suptitle("projections")
-    axs = axes.ravel()
-    k = 0
-    for p, angle in zip(projections, angles):
-        axs[k].imshow(p)
-        axs[k].set_title(f"{angle:.1f} raw")
-        k += 1
-        p[p < threshold] = 0
-        axs[k].imshow(p)
-        axs[k].set_title(f"{angle:.1f} above threshold")
-        # k += 1
-        # fimages = p.shape[1]
-        # p = cv.resize(
-        # p,
-        # (
-        # int(along_range/ortho_step),
-        # fimages,
-        # ),
-        # )
-        # axs[k].imshow(p.T/p.max() > 0.05)
 
-        k += 1
-        axs[k].imshow(p > threshold)
-        axs[k].set_title(f"{angle:.1f} threshold")
-        k += 1
-    # https://stackoverflow.com/questions/9295026/how-to-remove-axis-legends-and-white-padding
-    for a in axs:
-        a.set_axis_off()
+def get_rectified_projections(projections):  # , along_cells, ortho_cells):
+    rectified_projections, angles, ortho_cells = [], [], []
+    for angle in projections:
+        angles.append(angle)
+        # ri = projections[angle]["rectified_interpolation"].T
+        # rp = get_rectified_projection(ri, along_cells)
+        rp = projections[angle]["rectified_projection"]
+        rectified_projections.append(rp)
+        ortho_cells.append(rp.shape[1])
+    assert np.allclose(ortho_cells, np.mean(ortho_cells))
+    return rectified_projections, angles, ortho_cells[0]
 
 
 def get_max_bounding_ray(parameters):
@@ -372,7 +440,7 @@ def get_opti(directory, ext="obj"):
     print("directory", directory)
     assert ext in ["pcd", "obj"]
     opti_meshes = glob.glob(
-        os.path.join(os.path.dirname(args.directory), "opti", f"*careful_mm.{ext}")
+        os.path.join(os.path.dirname(directory), "opti", f"*careful_mm.{ext}")
     )
     print("opti_meshes", opti_meshes)
     winner = None
@@ -383,7 +451,9 @@ def get_opti(directory, ext="obj"):
             if "zoom_X" in item:
                 winner = item
     print("winner", winner)
-    opti = optical_alignment(directory=os.path.dirname(winner), name_pattern=os.path.basename(winner)[:-7])
+    opti = optical_alignment(
+        directory=os.path.dirname(winner), name_pattern=os.path.basename(winner)[:-7]
+    )
     return opti
 
 
@@ -398,8 +468,9 @@ def get_scan_start_angles(parameters):
 
 
 def main(args):
+    directory = os.path.realpath(args.directory)
     dea = diffraction_experiment_analysis(
-        directory=args.directory,
+        directory=directory,
         name_pattern=args.name_pattern,
     )
 
@@ -415,53 +486,43 @@ def main(args):
     reference_position = parameters["reference_position"]
     max_bounding_ray = get_max_bounding_ray(parameters)
 
-    oa = get_opti(args.directory)
+    oa = get_opti(directory)
     opti = oa.get_mesh_mm()
     opti.compute_vertex_normals()
     opti.paint_uniform_color(yellow)
-    
+
     assert len(seed_positions) == ntrigger
 
     tr = dea.get_tioga_results()
     lines = np.reshape(tr, (ntrigger, nimages))
     lines = lines.T
 
-    profiles = get_profiles(lines, scan_start_angles)
-
-    # reference_position = get_position_from_vector(
-    # np.mean(seed_positions, axis=0), keys=["CentringX", "CentringY", "AlignmentY"]
-    # )
-
-    projections = get_projections_from_profiles(
-        profiles, nimages, seed_positions, max_bounding_ray, reference_position, oa,
-    )
-
     along_max = seed_positions[:, -1].max()
     along_min = seed_positions[:, -1].min()
     along_range = along_max - along_min
+
+    along_cells = int(along_range / ortho_step)
+    print("along_cells", along_cells)
+
+    profiles = get_profiles(lines, scan_start_angles)
+
+    projections = get_projections_from_profiles(
+        profiles,
+        nimages,
+        seed_positions,
+        max_bounding_ray,
+        reference_position,
+        oa,
+        along_cells,
+        ortho_step,
+    )
+
     plot_projections(projections, ntrigger, nimages, along_step, ortho_step)
     pylab.show()
-    along_cells = int(along_range / ortho_step)
-    angles, rectified_interpolations, rectified_projections = [], [], []
-    for angle in projections:
-        angles.append(angle)
-        rectified_interpolations.append(projections[angle]["rectified_interpolation"].T)
 
-    ortho_cells = rectified_interpolations[0].shape[1]
-    print("ortho_cells", ortho_cells)
-    print("along_cells", along_cells)
-    for ri in rectified_interpolations:
-        rp = cv.resize(
-            ri,
-            (
-                ortho_cells,
-                along_cells,
-            ),
-        )
-        rectified_projections.append(rp)
-
-    # plot_rectified_projections(rectified_projections, angles, ntrigger, nimages, along_range, ortho_step, threshold=args.min_spots)
-    # pylab.show()
+    rectified_projections, angles, ortho_cells = get_rectified_projections(
+        projections
+    )  # , along_cells, ortho_cells)
 
     reconstruction = get_reconstruction(
         [p > args.min_spots for p in rectified_projections], angles
@@ -472,6 +533,9 @@ def main(args):
         reconstruction.max(),
         reconstruction.mean(),
     )
+
+    print("ortho_cells", ortho_cells)
+
     # _projections.shape (217, 5, 299)
     # reconstruction (shape, max, mean) (217, 598, 598) 5.0 0.4572133
     ortho_range = ortho_cells * ortho_step
@@ -497,14 +561,14 @@ def main(args):
     # print("origin_index (int)", origin_index)
 
     volume = get_volume_from_reconstruction(reconstruction, threshold=0.775)
-    vadt_px = get_mesh_px(volume)
+    vadt_px = get_mesh_px(volume, gradient_direction="ascent")
     vadt_px.paint_uniform_color(magenta)
-    directions = np.array([ 1,  1,  1])
-    #directions = np.array([1, 1, -1])  # *
-    #directions = np.array([ 1, -1,  1])
+    directions = np.array([1, 1, 1])
+    # directions = np.array([1, 1, -1])  # *
+    # directions = np.array([ 1, -1,  1])
     # directions = np.array([-1,  1,  1])
-    #directions = np.array([ 1, -1, -1]) # *
-    #directions = np.array([-1,  1, -1]) # *
+    # directions = np.array([ 1, -1, -1]) # *
+    # directions = np.array([-1,  1, -1]) # *
     # directions = np.array([-1, -1,  1])
     # directions = np.array([-1, -1, -1])
 
@@ -526,14 +590,14 @@ def main(args):
     o3d.visualization.draw_geometries(
         [opti, vadt_mm],
         window_name=f"{directions}",
-        #width=480,
-        #height=480,
-        #left=5,
-        #top=5,
-        #lookat=view["lookat"],
-        #up=view["up"],
-        #front=view["front"],
-        #zoom=view["zoom"],
+        # width=480,
+        # height=480,
+        # left=5,
+        # top=5,
+        # lookat=view["lookat"],
+        # up=view["up"],
+        # front=view["front"],
+        # zoom=view["zoom"],
     )
 
 
