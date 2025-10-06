3e4cb4d8bb6bf69e58c1a5adbc1d4734b270b35d
Martin Savko <savko@synchrotron-soleil.fr>
2025-10-06 17:58:52 +0200

    add note

---
+++ src/experimental_methods/__init__.py
@@ -0,0 +1,5 @@
+#https://www.foundationsafety.com/python-project-structure
+#from project_name.main import some_function
+#from project_name.subdirectory.in_subdir import some_other_function
+
+#__all__ = ["some_function", "some_other_function"]
