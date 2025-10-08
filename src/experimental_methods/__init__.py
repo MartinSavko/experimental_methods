c60a55adda452fa620222d0965c8116e6b876653
Martin Savko <savko@synchotron-soleil.fr>
2025-10-08 20:06:07 +0200

    update __init__.py

--- src/experimental_methods/__init__.py
+++ src/experimental_methods/__init__.py
@@ -4,17 +4,17 @@
 
 #__all__ = ["some_function", "some_other_function"]
 
-from experimental_methods.utils.history_saver import get_jpegs_from_arrays
+
 from experimental_methods.utils.speech import speech, defer
 
 from experimental_methods.instrument.goniometer import goniometer
-from experimental_methods.instrument.speaking_goniometer import speaking_goniometer, main as speaking_goniometer_cli
+from experimental_methods.instrument.speaking_goniometer import speaking_goniometer
 from experimental_methods.instrument.detector import detector
-from experimental_methods.instrument.oav_camera import oav_camera, main as oav_camera_cli
-from experimental_methods.instrument.axis_stream import axis_camera, main as axis_stream_cli
+from experimental_methods.instrument.oav_camera import oav_camera
+from experimental_methods.instrument.axis_stream import axis_camera
 from experimental_methods.instrument.camera import camera
 from experimental_methods.instrument.cats import cats, dewar_content
-from experimental_methods.instrument.transmission import transmission, transmission_mockup, main as transmission_cli
+from experimental_methods.instrument.transmission import transmission, transmission_mockup
 from experimental_methods.instrument.resolution import resolution, resolution_mockup
 from experimental_methods.instrument.energy import energy, energy_mockup
 from experimental_methods.instrument.flux import flux, flux_mockup
@@ -28,34 +28,52 @@ from experimental_methods.instrument.fast_shutter import fast_shutter
 from experimental_methods.instrument.motor import monochromator_rx_motor
 from experimental_methods.instrument.slits import slits1, slits2
 
-from experimental_methods.utils.image_monitor import main as image_monitor_cli
-from experimental_methods.utils.anneal import anneal, main as anneal_cli
+from experimental_methods.utils.anneal import anneal
 from experimental_methods.utils.raddose import raddose
 
 from experimental_methods.experiment.experiment import experiment
-from experimental_methods.experiment.udc import udc as udc_cli
-from experimental_methods.experiment.mechanized_sample_evaluation import main as mse_cli
-from experimental_methods.experiment.beam_align import beam_align, main as beam_align_cli
-from experimental_methods.experiment.slit_scan import slit_scan, main as slit_scan_cli
+from experimental_methods.experiment.beam_align import beam_align
+from experimental_methods.experiment.slit_scan import slit_scan
 from experimental_methods.experiment.scan_and_align import scan_and_align
-from experimental_methods.experiment.optical_alignment import optical_alignment, main as optical_alignment_cli
-from experimental_methods.experiment.omega_scan import omega_scan, main as omega_scan_cli
-from experimental_methods.experiment.inverse_scan import inverse_scan, main as inverse_scan_cli
-from experimental_methods.experiment.reference_images import reference_images, main as reference_images_cli
-from experimental_methods.experiment.helical_scan import helical_scan, main as helical_scan_cli
-from experimental_methods.experiment.raster_scan import raster_scan, main as raster_scan_cli
-from experimental_methods.experiment.diffraction_tomography import diffraction_tomography, main as diffraction_tomography_cli
-from experimental_methods.experiment.nested_helical_acquisition import nested_helical_acquisition, main as nested_helical_acquisition_cli
+from experimental_methods.experiment.optical_alignment import optical_alignment
+from experimental_methods.experiment.omega_scan import omega_scan
+from experimental_methods.experiment.inverse_scan import inverse_scan
+from experimental_methods.experiment.reference_images import reference_images
+from experimental_methods.experiment.helical_scan import helical_scan
+from experimental_methods.experiment.raster_scan import raster_scan
+from experimental_methods.experiment.diffraction_tomography import diffraction_tomography
+from experimental_methods.experiment.nested_helical_acquisition import nested_helical_acquisition
 from experimental_methods.experiment.tomography import tomography
 from experimental_methods.experiment.film import film
-from experimental_methods.experiment.fluorescence_spectrum import fluorescence_spectrum, main as fluorescence_spectrum_cli
-from experimental_methods.experiment.energy_scan import energy_scan, main as energy_scan_cli
-from experimental_methods.experiment.mount import mount, main as mount_cli
+from experimental_methods.experiment.fluorescence_spectrum import fluorescence_spectrum
+from experimental_methods.experiment.energy_scan import energy_scan
+from experimental_methods.experiment.mount import mount
 
-from experimental_methods.analysis.shape_from_vadt import main as shape_from_vadt
-
+from experimental_methods.utils.history_saver import get_jpegs_from_arrays
+from experimental_methods.experiment.omega_scan import main as omega_scan_cli
+from experimental_methods.experiment.reference_images import main as reference_images_cli
+from experimental_methods.experiment.inverse_scan import main as inverse_scan_cli
+from experimental_methods.experiment.helical_scan import main as helical_scan_cli
+from experimental_methods.experiment.raster_scan import main as raster_scan_cli
+from experimental_methods.experiment.diffraction_tomography import main as diffraction_tomography_cli
+from experimental_methods.experiment.nested_helical_acquisition import main as nested_helical_acquisition_cli
+from experimental_methods.experiment.fluorescence_spectrum import main as fluorescence_spectrum_cli
+from experimental_methods.experiment.energy_scan import main as energy_scan_cli
+from experimental_methods.experiment.optical_alignment import main as optical_alignment_cli
+from experimental_methods.experiment.slit_scan import main as slit_scan_cli
+from experimental_methods.experiment.beam_align import main as beam_align_cli
+from experimental_methods.experiment.mechanized_sample_evaluation import main as mse_cli
+from experimental_methods.experiment.udc import udc as udc_cli
+from experimental_methods.experiment.mount import main as mount_cli
+from experimental_methods.instrument.transmission import main as transmission_cli
+from experimental_methods.instrument.oav_camera import main as oav_camera_cli
+from experimental_methods.instrument.axis_stream import main as axis_stream_cli
 from experimental_methods.utils.vbpc import main as vbpc_cli
 from experimental_methods.utils.hbpc import main as hbpc_cli
+from experimental_methods.analysis.shape_from_vadt import main as shape_from_vadt_cli
+from experimental_methods.utils.image_monitor import main as image_monitor_cli
+from experimental_methods.utils.anneal import main as anneal_cli
+from experimental_methods.instrument.speaking_goniometer import main as speaking_goniometer_cli
 
 __all__ = [
     "speech",
