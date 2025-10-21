f8eac1804ea9fd2ebbdba396f48173d2d3308116
Martin Savko <savko@synchotron-soleil.fr>
2025-10-21 10:39:17 +0200

    Clean up the syntax. Make chunk size explicit and appropriate for each camera type (generation).

--- start_camera.sh
+++ start_camera.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-for k in 1 6 8 ; do axis_stream.py -c cam${k} -s cam${k} -o h264 & done
-axis_stream.py -c cam13 -s cam13 -o hevc &
-for m in 1 2 3 4 quad; do axis_stream.py -c cam14 -m ${m} -s cam14_${m} -o hevc & done
+
+for k in 1 6 8; do
+    axis_stream.py -c cam${k} -s cam${k} -o h264 -C 12 & 
+done
+
+axis_stream.py -c cam13 -s cam13 -o hevc -C 16 &
+
+for m in 1 2 3 4 quad; do 
+    axis_stream.py -c cam14 -m ${m} -s cam14_${m} -o hevc -C 16 & 
+done
+
