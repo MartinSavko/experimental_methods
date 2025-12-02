fbad1cb842cb99b3b4cb854c46592008a18a5aa0
Martin Savko <savko@synchotron-soleil.fr>
2025-12-02 12:52:15 +0100

    quick calibration (~23 min)

--- run_mk3_calibration.sh
+++ run_mk3_calibration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-target="tungsten_pin_42um"
-directory=/nfs/data4/2025_Run5/com-proxima2a/Commissioning/mk3_calibration/${target}_$(date +%Y%m%d_%H%M%S)
-angles="(1, 97, 179, 223, 313)"
+# target="tungsten_pin_42um"
+target="hampton_loop_with_sharp_crystal"
+directory=/nfs/data4/2025_Run5/com-proxima2a/Commissioning/mk3_calibration/$(date +%Y%m%d_%H%M%S)_${target}
+angles="(0,90,180,270)" #,223,313)"
 
 function my_align() {
     k=${1};
@@ -9,29 +10,34 @@ function my_align() {
     echo ${k} ${p};
     r=0;
     for z in 1 4 5; do
-        optical_alignment.py -d ${directory} -n ${k}_${p}_${r}_zoom_${z}_eager -K ${k} -P ${p} -r ${r} -z ${z} -b -A -C --extreme -a \"${angles}\";
+        optical_alignment.py -d ${directory} -n ${k}_${p}_${r}_${target}_zoom_${z}_eager -K ${k} -P ${p} -r ${r} -z ${z} -b -A -C --extreme -a ${angles};
     done;
-    r=360;
-    for z in 5; do 
-        echo ${k} ${p} ${z}; 
-        optical_alignment.py -d ${directory} -n ${k}_${p}_${r}_zoom_${z} -K ${k} -P ${p} -r ${r} -z ${z} -b -A -C --extreme; 
-        echo; 
-    done;
+#     r=360;
+#     for z in 5; do 
+#         echo ${k} ${p} ${z}; 
+#         optical_alignment.py -d ${directory} -n ${k}_${p}_${r}_zoom_${z} -K ${k} -P ${p} -r ${r} -z ${z} -b -A -C --extreme; 
+#     done;
+    echo; echo; echo; echo; echo; 
 }
 
-k=0
-for p in {0..360..10}; do
-    my_align ${k} ${p}
+p=0;
+for k in {0..240..15}; do 
+    time my_align ${k} ${p}
 done
 
-p=0
-for k in {0..240..10}; do 
-    my_align ${k} ${p}
+k=0;
+for p in {1..361..30}; do
+    time my_align ${k} ${p}
 done
+# 
+# p=90;
+# for k in {0..240..10}; do 
+#     time my_align ${k} ${p}
+# done
 
-for k in 23 37 47 67 97 127 181; do
-    for p in 89 181 227 317; do 
-        my_align ${k} ${p}
-    done
-done
+#for k in 23 37 47 67 97 127 181; do
+#    for p in 89 179 227 317; do 
+#        time my_align ${k} ${p}
+#    done
+#done
 
