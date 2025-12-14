d23bca3b91783b66b84076498ee14379344d1e83
Martin Savko <savko@synchotron-soleil.fr>
2025-12-14 14:11:39 +0100

    base most_likely_click on crystal class only, go for foreground extreme otherwise

--- murko.py
+++ murko.py
@@ -2545,7 +2545,7 @@ def get_most_likely_click(predictions, k=0, verbose=False, min_size=32):
     gmlc = False
     most_likely_click = -1, -1
 
-    for notion in ["crystal", "loop_inside", "loop"]:
+    for notion in ["crystal"]: #, "loop_inside", "loop"]:
         notion_prediction = get_notion_prediction(
             predictions, notion, k=k, min_size=min_size
         )
@@ -2769,7 +2769,7 @@ def get_most_likely_click_from_description(description, verbose=False):
     gmlc = False
     most_likely_click = -1, -1
     shape = np.array(description["hierarchical_mask"].shape)
-    for notion in ["crystal", "loop_inside", "loop"]:
+    for notion in ["crystal"]: #, "loop_inside", "loop"]:
         notion_description = description[get_notion_string(notion)]
         if notion_description["present"]:
             if "distance_transform" in notion_description:
