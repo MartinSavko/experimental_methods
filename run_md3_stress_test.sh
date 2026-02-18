0cf45b3b7434feb7a704f40564b9c0ea259ac6d4
Martin Savko <savko@synchotron-soleil.fr>
2026-02-18 12:44:27 +0100

    faster stress test defaults

--- run_md3_stress_test.sh
+++ run_md3_stress_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-n=100
-base_path=/nfs/data4/2026_Run1/com-proxima2a/md3_stress_test/20260116_a
-for e in 36 18 9 4.5 3.6 1.8 1 1.8 3.6 4.5 9 18 36; do
+n=50
+base_path=/nfs/data4/2026_Run1/com-proxima2a/md3_stress_test/20260117_a
+for e in 9 4.5 3.6 1.8 1 1.8 3.6 4.5 9; do
     echo ${e} 
     ./goniometer.py -n ${n} --random --scan_exposure_time ${e} --base_path ${base_path}
 done
