b01dea979656cac3da0a3ecf2ea9e4dc218e1719
Martin Savko <savko@synchotron-soleil.fr>
2026-03-31 19:13:27 +0200

    more robust determination of the first and the last image in the series

--- compare_first_and_last.sh
+++ compare_first_and_last.sh
@@ -7,10 +7,10 @@ echo ${start}
 end=$(ls -tr1 | grep -v png | grep -v txt | tail -n 1); 
 #end=20260216_140227; #220260209_16552; #10260209_164003;
 echo ${end}; 
-first=${start}/before_Omega_at_${a}_1.jpg; 
-echo ${first}; 
-last=$(ls ${end}/after_Omega_at_${a}_*.jpg -tr1 | tail -n 1); 
-echo ${last}; 
+first=$(ls ${start}/*before_Omega_at_${a}.jpg -tr1 | head -n 1); 
+echo first ${first}; 
+last=$(ls ${end}/*after_Omega_at_${a}.jpg -tr1 | tail -n 1); 
+echo last ${last}; 
 echo eog -n ${first} ${last}; 
 eog -n ${first} ${last}
 
