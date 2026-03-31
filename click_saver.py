6e462019904e7d7e4b9f773104899ed338e9b37c
Martin Savko <savko@synchotron-soleil.fr>
2026-03-31 19:12:57 +0200

    Use argparse instead of optparse Encode only click into the filename (kappa, phi and omega are supposed to be already in the basename)

--- click_saver.py
+++ click_saver.py
@@ -13,17 +13,33 @@ try:
 except ImportError:
     import complexjpeg as simplejpeg
 
+from useful_routines import get_vector_from_position
 
-def main():
-    import optparse
+def main(
+    motor_names=[
+        "AlignmentX",
+        "AlignmentY",
+        "AlignmentZ",
+        "CentringX",
+        "CentringY",
+        "Kappa",
+        "Phi",
+        "Omega",
+    ]
+):
+    import argparse
 
-    parser = optparse.OptionParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_option("-d", "--directory", type=str, help="directory")
-    parser.add_option("-n", "--name_pattern", type=str, help="filename template")
-    parser.add_option("-y", "--click_y", type=float, help="click y")
-    parser.add_option("-x", "--click_x", type=float, help="click x")
-    parser.add_option(
+
+    parser.add_argument("-d", "--directory", type=str, help="directory")
+    parser.add_argument("-n", "--name_pattern", type=str, help="filename template")
+    parser.add_argument("-y", "--click_y", type=float, help="click y")
+    parser.add_argument("-x", "--click_x", type=float, help="click x")
+    parser.add_argument("-o", "--omega", default=None, type=float, help="omega")
+    parser.add_argument(
         "-t",
         "--timestamp",
         default=None,
@@ -31,42 +47,40 @@ def main():
         help="if specified will look for image in history corresponding to the timestamp instead of grabing a new one",
     )
 
-    options, args = parser.parse_args()
+    args = parser.parse_args()
 
     s = time.time()
 
     cam = oav_camera()
     g = goniometer()
 
-    click = np.array([options.click_y, options.click_x]).astype(int)
-    if options.timestamp != None:
-        image = cam.get_image_corresponding_to_timestamp(options.timestamp)
+    click = np.array([args.click_y, args.click_x]).astype(int)
+    if args.timestamp != None:
+        image = cam.get_image_corresponding_to_timestamp(args.timestamp)
     else:
         image = cam.get_image()
+        
     zoom = cam.get_zoom()
     calibration = cam.get_calibration()
     position = g.get_aligned_position(
-        motor_names=[
-            "AlignmentY",
-            "AlignmentZ",
-            "CentringX",
-            "CentringY",
-            "Omega",
-            "Kappa",
-            "Phi",
-        ]
+        motor_names=motor_names
     )
+    if args.omega is not None:
+        position["Omega"] = args.omega
+        
     e = time.time()
 
     print("image read in %.3f seconds" % (e - s))
 
-    if not os.path.isdir(options.directory):
-        os.makedirs(options.directory)
+    if not os.path.isdir(args.directory):
+        os.makedirs(args.directory)
 
     s = time.time()
-    fname_base = os.path.join(options.directory, options.name_pattern)
-    image_filename = f'{fname_base}_ay_{position["AlignmentY"]:.4f}_az_{position["AlignmentZ"]:.4f}_cx_{position["CentringX"]:.4f}_cy_{position["CentringY"]:.4f}_omega_{position["Omega"]:.3f}_kappa_{position["Kappa"]:.3f}_phi_{position["Phi"]:.3f}_zoom_{int(zoom):d}_y_{click[0]:d}_x_{click[1]:d}.jpg'
+    fname_base = os.path.join(args.directory, args.name_pattern)
+    #image_filename = f'{fname_base}_ax_{position["AlignmentX"]:.3f}_ay_{position["AlignmentY"]:.3f}_az_{position["AlignmentZ"]:.3f}_cx_{position["CentringX"]:.3f}_cy_{position["CentringY"]:.3f}_y_{click[0]:d}_x_{click[1]:d}.jpg'
 
+    image_filename = f'{fname_base}_y_{click[0]:d}_x_{click[1]:d}.jpg'
+    
     # image_filename = "%s_zoom_%d_y_%d_x_%d.jpg" % (
     # fname_base,
     # ay,
@@ -84,7 +98,7 @@ def main():
     cam.save_image(image_filename)
 
     double_click_file = h5py.File(
-        "%s.h5" % os.path.join(options.directory, options.name_pattern), "w"
+        "%s.h5" % os.path.join(args.directory, args.name_pattern), "w"
     )
 
     # dt = h5py.special_dtype(vlen=np.dtype('uint8'))
@@ -104,6 +118,8 @@ def main():
     double_click_file.create_dataset("click", data=click)
 
     double_click_file.create_dataset("calibration", data=calibration)
+    
+    double_click_file.create_dataset("position", data=get_vector_from_position(position, keys=motor_names))
 
     double_click_file.close()
 
